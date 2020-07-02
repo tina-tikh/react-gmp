64f6ec3bf27a353f7d0619defce0f52cedce0b6b
Khrystsina Tsikhanovich <xtinakot@gmail.com>
2020-07-02 14:43:22 +0300

    feat: add Back to Search button

--- src/components/film/Film.tsx
+++ src/components/film/Film.tsx
@@ -9,6 +9,7 @@ import { Movie } from '../../api';
 import { AppState, getSelectedMovie, getSimilarMovies } from '../../store';
 import FilmList from '../FilmList';
 import FilmDetails from './FilmDetails';
+import { LinkButton } from '../form';
 
 interface FilmProps extends RouteComponentProps<{ id: string }> {
   similar: Movie[],
@@ -44,6 +45,7 @@ class Film extends Component<FilmProps, FilmState> {
     return (
       <React.Fragment>
         <Header>
+          <LinkButton onClick={() => this.handleBackToSearch()}>Back to search</LinkButton>
           <FilmDetails film={this.props.movie}/>
         </Header>
         <Main>
@@ -56,6 +58,10 @@ class Film extends Component<FilmProps, FilmState> {
         </Main>
       </React.Fragment>
     );
+  }
+
+  handleBackToSearch(): void {
+    this.props.history.goBack();
   }
 }
 
