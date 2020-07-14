4157707ffed62e5fb3228b9026d8f815831868e5
Khrystsina Tsikhanovich <xtinakot@gmail.com>
2020-07-14 21:33:05 +0300

    style: prettier

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,13 @@
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       use: [
         {
           loader: require.resolve('ts-loader'),
-        }
+        },
       ],
     });
     config.resolve.extensions.push('.ts', '.tsx');
